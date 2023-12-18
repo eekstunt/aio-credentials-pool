46100b88175139e6ee06e973f9c608007d8675d4
evgenii-korotaev <evgenii@intelas.com>
2023-12-18 19:11:01 +0000

    Fix formatting

--- aio_credentials_pool/tests/test_persistent_credentials_pool.py
+++ aio_credentials_pool/tests/test_persistent_credentials_pool.py
@@ -19,10 +19,7 @@ async def engine(database_name):
     connection = await asyncpg.connect(user=url.username, password=url.password, host=url.host)
 
     try:
-        databases = await connection.fetch(
-            'SELECT datname FROM pg_database WHERE datname=$1',
-            database_name
-        )
+        databases = await connection.fetch('SELECT datname FROM pg_database WHERE datname=$1', database_name)
         if not any(database_name in db['datname'] for db in databases):
             await connection.execute('CREATE DATABASE $1', database_name)
             logging.info(f'Database {database_name} created successfully.')
@@ -33,7 +30,6 @@ async def engine(database_name):
         make_url(POSTGRES_URL).set(database=database_name),
     )
     yield test_engine
-
 
 
 @pytest_asyncio.fixture()
